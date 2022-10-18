a9bb1647df66c2d5829a4569b493d7e7bf2d8c1a
danrino95 <danrino95@gmail.com>
2022-10-18 10:13:47 +0530

    configured github login.15

--- themes/frontend/views/home.blade.php
+++ themes/frontend/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="container">
+    <!-- <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -28,5 +28,27 @@
 				@endforeach
             </div>
         </div>
+    </div> -->
+    <link rel="stylesheet" href="http://atelierspot.com/style.css" />
+    <div class="container-fluid fixed-top header disable-selection">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col">
+              <h1><strong>Atelier Spot</strong></h1>
+              <h3><strong>Coming Soon</strong></h3>
+              <p class="small">– Do It All –</p>
+              <!--.btn.btn-danger(href='#',role='button', onclick='cameraSet()') + ADD LINE-->
+            </div>
+          </div>
+        </div>
+        <div class="col"></div>
+      </div>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/89/three.min.js"></script>
+    <script src="https://github.com/mrdoob/three.js/blob/master/src/constants.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
+    <script src="http://atelierspot.com/script.js"></script>
 </x-app-layout>
