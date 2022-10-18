d9ab5fd0f7a6983c3e73ade62a1da4e3ec75cbdf
danrino95 <danrino95@gmail.com>
2022-10-18 08:51:27 +0530

    configured github login.6

--- app/Http/Controllers/GithubSocialiteController.php
+++ app/Http/Controllers/GithubSocialiteController.php
@@ -34,15 +34,46 @@ class GithubSocialiteController extends Controller
      */
     public function handleProviderCallback(Request $request)
     {
-        $user = Socialite::driver('github')->user();
-
-        Session::put('user', $user);
-
-        $redirect = $request->input('redirect');
-        if($redirect)
-        {
-            return redirect($redirect);
+        try {
+     
+            $user = Socialite::driver('github')->user();
+      
+            $finduser = User::where('social_id', $user->id)->first();
+      
+            if($finduser){
+      
+                Auth::login($finduser);
+     
+                return redirect('/home');
+      
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'social_id'=> $user->id,
+                    'social_type'=> 'github',
+                    'password' => encrypt('my-github')
+                ]);
+     
+                Auth::login($newUser);
+      
+                return redirect('/home');
+            }
+     
+        } catch (Exception $e) {
+            dd($e->getMessage());
         }
-        return 'GitHub auth successful. Now navigate to a demo.';
+
+        // 
+        // $user = Socialite::driver('github')->user();
+
+        // Session::put('user', $user);
+
+        // $redirect = $request->input('redirect');
+        // if($redirect)
+        // {
+        //     return redirect($redirect);
+        // }
+        // return 'GitHub auth successful. Now navigate to a demo.';
     }
 }
