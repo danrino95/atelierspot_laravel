43f36c28d66165eea056324ab6ff3cc93a21a3b7
danrino95 <danrino95@gmail.com>
2022-10-18 11:32:37 +0530

    fixed styling and added three js to home3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <h1 class="text-center" style="letter-spacing: 30px;font-size:30px"><strong>SECLA</strong></h1>
+                <h1 class="text-center" style="letter-spacing: 30px;font-size:60px"><strong>SECLA</strong></h1>
                 </div>
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 <h5 style="letter-spacing: 30px;"><b>BY | ATELIERSPOT</b></h5>
