6b8c24a184b6dbc314726bed6f6fe7e20e7a10f5
danrino95 <danrino95@gmail.com>
2022-10-18 10:45:27 +0530

    fixed styling and added three js to home3

--- resources/views/dashboard.blade copy.php
+++ resources/views/dashboard.blade copy.php
@@ -1,3 +1,10 @@
+<!-- <x-app-layout> -->
+    <!-- <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot> -->
+
     <link rel="stylesheet" href="http://atelierspot.com/style.css" />
     <div class="container-fluid fixed-top header disable-selection">
       <div class="row">
@@ -9,6 +16,7 @@
               <h1><strong>Atelier Spot</strong></h1>
               <h3><strong>Coming Soon</strong></h3>
               <p class="small">– Do It All –</p>
+              <!--.btn.btn-danger(href='#',role='button', onclick='cameraSet()') + ADD LINE-->
             </div>
           </div>
         </div>
@@ -20,3 +28,4 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
     <script src="http://atelierspot.com/script.js"></script>
+<!-- </x-app-layout> -->
